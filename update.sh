dfe64c5a1d2d7499c3a209975a196a51cbf0b0e6
Sky-ey <lyfylp@126.com>
2025-03-28 12:28:53 +0800

    Add CSV file

--- update.sh
+++ update.sh
@@ -5,21 +5,21 @@ set -e
 # cwd to script directory
 cd "$(dirname "$0")"
 
-fetch_data() {
-  for _ in $(seq 1 10); do
-    # fetch the data
+fetch_json_data() {
+  for _ in $(seq 1 5); do
+    # fetch the JSON data
     data=$(curl -sL https://data.phishtank.com/data/online-valid.json)
 
     # check if curl was successful
     if [[ $? -ne 0 ]]; then
-      echo "Curl failed. Retrying..."
+      echo "JSON curl failed. Retrying..."
       sleep 60
       continue
     fi
 
     # check if data is non-empty
     if [[ -z "$data" ]]; then
-      echo "Received empty response. Retrying..."
+      echo "Received empty JSON response. Retrying..."
       sleep 60
       continue
     fi
@@ -40,12 +40,45 @@ fetch_data() {
   return 1
 }
 
-echo "Fetching latest data from phishtank..."
-if ! fetch_data; then
-  echo "Failed to fetch data after multiple attempts."
-  exit 1
+fetch_csv_data() {
+  for _ in $(seq 1 5); do
+    # fetch the CSV data
+    csv_data=$(curl -sL https://data.phishtank.com/data/online-valid.csv)
+
+    # check if curl was successful
+    if [[ $? -ne 0 ]]; then
+      echo "CSV curl failed. Retrying..."
+      sleep 60
+      continue
+    fi
+
+    # check if data is non-empty
+    if [[ -z "$csv_data" ]]; then
+      echo "Received empty CSV response. Retrying..."
+      sleep 60
+      continue
+    fi
+
+    return 0
+  done
+
+  return 1
+}
+
+echo "Fetching latest JSON data from phishtank..."
+if ! fetch_json_data; then
+  echo "Failed to fetch JSON data after multiple attempts."
+else
+  echo "Saving JSON data to hosts.json..."
+  echo "$data" > hosts.json
+  echo "Done, saved to hosts.json"
 fi
 
-echo "Saving data to hosts.json..."
-echo "$data" > hosts.json
-echo "Done, saved to hosts.json"
+echo "Fetching latest CSV data from phishtank..."
+if ! fetch_csv_data; then
+  echo "Failed to fetch CSV data after multiple attempts."
+else
+  echo "Saving CSV data to hosts.csv..."
+  echo "$csv_data" > hosts.csv
+  echo "Done, saved to hosts.csv"
+fi
